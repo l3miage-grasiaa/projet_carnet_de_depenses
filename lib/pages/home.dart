5566ba4d0a1fe63a6aa321f982ec8b6e2b0574e8
l3miage-grasiaa <Angelica-Rivera.Grasia@etu.univ-grenoble-alpes.fr>
2026-04-27 17:47:55 +0200

    .\Finish: Revisi TP Dasar UI dan Aset

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,12 +8,21 @@ class MyHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("TP1"),), // Parent utama halaman
       body: Center( // Menengahkan isinya
-          child: Column( // Menyusun vertikal
-            mainAxisAlignment: MainAxisAlignment.center, // Memusatkan isi column
-            children: [
-              const Text("Selamat tahun baru"), // Widget anak 1
-              Image.asset('assets/happyNewYear.gif') // Widget anak 2
-            ],
+          child: Container( // Container sebagai "pembungkus" luar
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(10.0), // Jarak antara bingkai dan isi
+            color: Colors.amber[100],
+
+            // Gunakan properti child (tunggal) untuk memasukkan Column
+            child: Column( // Menyusun vertikal
+              mainAxisSize: MainAxisSize.min, // // Agar column tidak menghabiskan seluruh layar
+              children: [
+                const Text(
+                    "Selamat tahun baru",
+                    style: TextStyle(fontWeight: FontWeight.bold),), // Widget anak 1
+                Image.asset('assets/happyNewYear.gif') // Widget anak 2
+              ],
+            ),
           )
       ),
     );
